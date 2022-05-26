120e7953660d23b8672197932aafab84de150474
Keita <askeita@yahoo.com>
2022-05-26 22:46:02 +0300

    0102

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,15 +2,37 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
+function useLocalStorageState(key, defaultValue) {
+  const [state, setState] = React.useState(
+    () => {
+      const valueIn = window.localStorage.getItem(key)
+      if (valueIn) {
+        return JSON.parse(valueIn)
+      }
+      return defaultValue
+    }
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
+
+  return [state, setState]
+}
 
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = useLocalStorageState('name', initialName)
+  // const value = window.localStorage.getItem('name')
+  // const [name, setName] = React.useState(() => value ?? initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
@@ -27,7 +49,15 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  const [count2, setCount] = useLocalStorageState('coun', 90)
+  return (
+    <>
+  <button onClick={() => setCount(c => c +1)}>
+     {count2}
+  </button>
+  <Greeting />
+  </>
+  )
 }
 
 export default App
